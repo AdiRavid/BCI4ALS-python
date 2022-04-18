c0805590fa40bcff680d24760b5e3d16a2d0df59
Maysan Bader <maysan.bader@mail.huji.ac.il>
2022-04-18 21:50:58 +0300

    change bg

--- new_bci_framework/ui/car dodge game PYTHON/race.py
+++ new_bci_framework/ui/car dodge game PYTHON/race.py
@@ -31,7 +31,7 @@ class CarRacer:
         gameIcon = pygame.image.load('carIcon.png')
         pygame.display.set_icon(gameIcon)
 
-        self.backgroundImage = pygame.image.load("background.png")
+        self.backgroundImage = pygame.image.load("supermarket1.jpeg")
         self.backgroundImage = pygame.transform.scale(self.backgroundImage, (800, 600))
         self.gameDisplay.blit(self.backgroundImage, (0, 0))
 
@@ -213,6 +213,7 @@ class CarRacer:
 
         pygame.mixer.music.load('bgmusic.mp3')
         pygame.mixer.music.play(-1)
+        pygame.mixer.music.set_volume(0.2)
         ############
         x = (self.display_width * 0.45)
         y = (self.display_height * 0.8)
